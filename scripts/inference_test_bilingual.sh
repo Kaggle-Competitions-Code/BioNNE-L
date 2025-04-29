85b55060ed75eb758897df07cb66b8e39b3d8645
EeyoreLee <li.chunyu0412@gmail.com>
2025-04-29 04:25:04 -0400

    bilingual script

---
+++ scripts/inference_test_bilingual.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../src
+CUDA_VISIBLE_DEVICES=0 python inference.py \
+--checkpoints_path /media/f/lichunyu/BioNNE-L/data/checkpoints/SapBERT-UMLS-2020AB-all-lang-from-XLMR-large-special_epoch_4_acc_0.5467.bin \
+--model_name_or_path /media/f/lichunyu/models/SapBERT-UMLS-2020AB-all-lang-from-XLMR-large-special \
+--tokenizer_name_or_path /media/f/lichunyu/models/SapBERT-UMLS-2020AB-all-lang-from-XLMR-large-special \
+--retrieval_data_path /media/f/lichunyu/BioNNE-L/data/eeyore/bilingual/test_data_sap_50.pkl \
+--lang bilingual \
+--save_file_name test_ru_predictions.tsv \
+--save_dir /media/f/lichunyu/BioNNE-L/data/eeyore/predictions
--- scripts/train_step2_bilingual.sh
+++ scripts/train_step2_bilingual.sh
@@ -2,12 +2,14 @@
 
 cd ../src
 CUDA_VISIBLE_DEVICES=0 python step2_train.py \
---train_data_path /media/f/lichunyu/BioNNE-L/data/eeyore/en/train_data_with_extra_sap_50.pkl \
+--train_data_path /media/f/lichunyu/BioNNE-L/data/eeyore/bilingual/train_data_sap_50.pkl \
+--dev_data_path /media/f/lichunyu/BioNNE-L/data/eeyore/bilingual/dev_data_sap_50.pkl \
 --retrieval_topk 5 \
---train_batch_size 8 \
---eval_batch_size 8 \
+--train_batch_size 4 \
+--eval_batch_size 4 \
 --learning_rate 1e-5 \
 --num_train_epochs 5 \
---model_name_or_path /media/f/lichunyu/models/BiomedNLP-BiomedBERT-base-uncased-abstract-special \
---tokenizer_name_or_path /media/f/lichunyu/models/BiomedNLP-BiomedBERT-base-uncased-abstract-special \
+--model_name_or_path /media/f/lichunyu/models/SapBERT-UMLS-2020AB-all-lang-from-XLMR-large-special \
+--tokenizer_name_or_path /media/f/lichunyu/models/SapBERT-UMLS-2020AB-all-lang-from-XLMR-large-special \
+--save_dir /media/f/lichunyu/BioNNE-L/data/checkpoints/bilingual \
 --do_eval
